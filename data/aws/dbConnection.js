f5ff8c90d7493f7c405e60ebbde4c080c36ce4f3
Jorge Gallego <gallegojorge908@gmail.com>
2022-12-23 08:36:45 -0500

    Testing GH Actions

--- data/aws/dbConnection.js
+++ data/aws/dbConnection.js
@@ -11,7 +11,7 @@ const connection = new Sequelize({
 });
 
 (async () => {
-	if ( process.env.ENV !== "actions") await connection.authenticate();
+	await connection.authenticate();
 })();
 
 module.exports = connection;

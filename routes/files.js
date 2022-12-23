6f819db9d5c4c1cabcec4e07ba521b2e6bd829b5
Jorge Gallego <gallegojorge908@gmail.com>
2022-12-23 09:55:12 -0500

    Added rate limiting middleware function to static SEO files endpoints

--- routes/files.js
+++ routes/files.js
@@ -1,6 +1,9 @@
 const { Router } = require("express");
 const { join } = require("path");
+const filesGetRequestLimiter = require("../middleware/rateLimit/filesGetRequestLimiter");
 const router = Router();
+
+router.use(filesGetRequestLimiter);
 
 router.get("/robots.txt", (req, res) => {
 	res.sendFile(join(__dirname, "..", "robots.txt"));

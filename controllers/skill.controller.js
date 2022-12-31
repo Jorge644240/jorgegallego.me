f08fb96f769456d99308854cad98bab0b9554a0a
Jorge Gallego <gallegojorge908@gmail.com>
2022-12-30 19:38:12 -0500

    Updated getAllSkills() method to sort by skill level and name

--- controllers/skill.controller.js
+++ controllers/skill.controller.js
@@ -2,7 +2,12 @@ const Skill = require("../models/skill.model")
 
 module.exports = class SkillController {
 	static async getAllSkills() {
-		return await Skill.findAll();
+		return await Skill.findAll({
+			order: [
+				["level", "DESC"],
+				["name", "ASC"]
+			]
+		});
 	}
 	static async getSkillsByLevel(skillLevel) {
 		return await Skill.findAll({
